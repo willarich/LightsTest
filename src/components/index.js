d5160f6036ee1dcd12d8bc98d41b8890a8e35002
William Richardson <willarich@gmail.com>
2021-07-06 22:56:03 -0700

    init commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import DropDown from "./Dropdown.vue";
+import Parallax from "./Parallax.vue";
+import Pagination from "./Pagination.vue";
+import Slider from "./Slider.vue";
+import Badge from "./Badge.vue";
+import NavTabsCard from "./cards/NavTabsCard.vue";
+import LoginCard from "./cards/LoginCard.vue";
+import HelloCard from "./cards/HelloCard.vue";
+import Tabs from "./Tabs.vue";
+import Modal from "./Modal.vue";
+
+export {
+  DropDown,
+  Parallax,
+  Pagination,
+  Slider,
+  Badge,
+  NavTabsCard,
+  LoginCard,
+  HelloCard,
+  Tabs,
+  Modal
+};
